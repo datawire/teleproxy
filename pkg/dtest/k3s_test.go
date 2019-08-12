e1efa34524a57989839eb678fa3003ce55d89222
Rafael Schloming <rhs@alum.mit.edu>
2019-08-12 13:25:05 -0400

    grab the machine lock so we don't conflict with nat tests

--- pkg/dtest/k3s_test.go
+++ pkg/dtest/k3s_test.go
@@ -1,10 +1,19 @@
 package dtest
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestMain(m *testing.M) {
+	// we get the lock to make sure we are the only thing running
+	// because the nat tests interfere with docker functionality
+	WithMachineLock(func() {
+		os.Exit(m.Run())
+	})
+}
 
 func TestContainer(t *testing.T) {
 	id := dockerUp("dtest-test-tag", "nginx")
